12f2b2814312fb9561e831e8280b0824566eef7f
KevVo <kevinvo246@hotmail.com>
2023-01-13 01:20:11 -0800

    Styling

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,10 +1,4 @@
 export type AmplifyDependentResourcesAttributes = {
-<<<<<<< HEAD
-  "api": {
-    "binqr": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string"
-=======
     "api": {
         "binqr": {
             "GraphQLAPIIdOutput": "string",
@@ -45,42 +39,5 @@ export type AmplifyDependentResourcesAttributes = {
             "Region": "string",
             "Arn": "string"
         }
->>>>>>> 1d2c050 (pull backend)
     }
-  },
-  "auth": {
-    "binqr0c8b1dc9": {
-      "AppClientID": "string",
-      "AppClientIDWeb": "string",
-      "CreatedSNSRole": "string",
-      "IdentityPoolId": "string",
-      "IdentityPoolName": "string",
-      "UserPoolArn": "string",
-      "UserPoolId": "string",
-      "UserPoolName": "string"
-    }
-  },
-  "function": {
-    "binQrGen": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    }
-  },
-  "geo": {
-    "map08f4aa31": {
-      "Arn": "string",
-      "Name": "string",
-      "Region": "string",
-      "Style": "string"
-    }
-  },
-  "storage": {
-    "s395f81f89": {
-      "BucketName": "string",
-      "Region": "string"
-    }
-  }
 }
