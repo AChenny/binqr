7ffa3e305e36350e2a0de0e782de0599cf71aba3
KevVo <kevinvo246@hotmail.com>
2023-01-12 22:10:29 -0800

    Added new reset button behind a auth wall

--- src/components/QrScanPage.js
+++ src/components/QrScanPage.js
@@ -1,24 +1,39 @@
-import React from 'react'
+import React, {Fragment} from 'react';
 import { updateQrEntry } from '../graphql/mutations';
-import { API, graphqlOperation } from 'aws-amplify'
 import { Button } from '@mui/material';
 import './QrScanPage.css'
+import { Amplify, API, graphqlOperation } from 'aws-amplify'
+import { Authenticator } from '@aws-amplify/ui-react';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import '@aws-amplify/ui-react/styles.css';
+
+import awsExports from "../aws-exports";
+Amplify.configure(awsExports);
 
 function QrScanPage() {
   const params = new URLSearchParams(window.location.search);
   const id = params.get('id');
 
   // Full Reporting Button
-  async function fullReport (id) {
+  async function fullReport (id, value) {
     try {
       const entry = {
         id: id,
-        full: true
+        full: value
       };
 
       await API.graphql(graphqlOperation(updateQrEntry, {input: entry}));
 
-      alert('Thank you for reporting!')
+      if (value) {
+        alert('Thank you for reporting!')
+      }
+      else {
+        alert("Bin " + id + " has been reset.")
+      }
     } catch (err) {
       console.log("Error reporting full bin", err)
     }
@@ -26,15 +41,26 @@ function QrScanPage() {
   
 
   return (
+    <Fragment>
     <div class='page-container'>
       <h3>Bin ID: {id}</h3>
-      <h2 className='scan-header'>Is this bin full or is this regarding another issue?</h2>
+      <div>Is this bin full or is this regarding another issue?</div>
       <div>
-        <Button variant='contained' sx={{px: 4, py:2, margin: 2}} onClick={()=> fullReport(id)} >Full</Button>
+        <Button variant='contained' sx={{px: 4, py:2, margin: 2}} onClick={()=> fullReport(id, true)} >Full</Button>
         <Button variant='contained' sx={{px: 4, py:2, margin: 2}}>Another Issue</Button>
       </div>
     </div>
-
+    <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />} >
+          <Typography>Please expand to access administrative controls</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Authenticator hideSignUp={true}>
+            <button onClick={() => fullReport(id, false)}>Reset</button> 
+          </Authenticator>
+        </AccordionDetails>
+      </Accordion>
+    </Fragment>
     
   )
 }
